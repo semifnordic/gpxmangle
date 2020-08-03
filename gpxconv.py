19cd07d3cbfe659f4e655bdc0c1ab3f678d3981b
semifnordic <semifnordic@gmail.com>
2020-08-03 14:35:21 -0400

    initial commit

---
+++ gpxconv.py
@@ -0,0 +1,25 @@
+import xml.etree.ElementTree as ET
+import os
+import argparse
+
+unitconv_const = 1.94384
+xmlns = '{http://www.topografix.com/GPX/1/1}'
+
+parser = argparse.ArgumentParser()
+parser.add_argument("gpxin", help="GPX input file to convert")
+args = parser.parse_args()
+
+#Check for presence of gpx file
+if not os.path.isfile(args.gpxin):
+    print("Cannot open " + args.gpxin)
+    exit()
+
+with open(args.gpxin, 'r') as f:
+    tree = ET.parse(f)
+    for point in tree.iter(xmlns+'trkpt'):
+        convspeed = float(point.find(xmlns+'sog').text) * unitconv_const
+        se = ET.SubElement(point, xmlns+'speedy')
+        se.text = str(convspeed)
+
+    tree.write('output.xml')
+  
